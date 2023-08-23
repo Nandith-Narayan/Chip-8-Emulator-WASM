62ffbeb3f67bc01a66aaa0e2be779ef41d929ae2
Nandith-Narayan <nandith2ndemail@gmail.com>
2023-08-23 00:11:39 -0400

    Fixed import

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import init, { greet } from "./pkg/hello_wasm.js";
-      init().then(() => {
-        greet("WebAssembly");
-      });
+import init, {
+    greet
+}
+from "./pkg/chip_8_wasm.js";
+init().then(() => {
+    greet("WebAssembly");
+});
